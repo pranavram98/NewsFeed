e25901823c73d347d9278e4884fe9d2e0f8bd2fa
Pranav R <pranav.r@C-LM-0326.local>
2019-12-23 14:03:18 +0530

    added segment

--- src/components/ArticleDetail.js
+++ src/components/ArticleDetail.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import './Segment.css';
-
+import './Segment.css'
 const ArticleDetail=({article})=>{
     if(!article){
         return(
@@ -11,16 +10,16 @@ const ArticleDetail=({article})=>{
 );
     }
 return(
-<div>
-<h2 className="ui header" id="articletitle" >{article.title}</h2>
+<div>  
+<div className="ui inverted segment">
+<h2 className="ui header" id="articletitle">{article.title}</h2>
 <h3 className="ui header" id="article">{article.description}</h3>
 <h5 className="ui header" id="author">- {article.author}</h5>
 <div className="ui divider"></div>
-
 <img className="ui large image" src={article.urlToImage} alt="" id="detailimg"/><br/>
 <p id="article">{article.content}</p>
-
 </div>
+ </div>
     );
 
 };
---
+++ src/components/FullArticle.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const FullArticle=()=>{
+    return<div>HELLO</div>
+};
+export default FullArticle;
