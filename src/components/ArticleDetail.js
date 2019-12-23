c461bae03e70a5d4f32a18873d2ad1ca804d3eda
Pranav R <pranav.r@C-LM-0326.local>
2019-12-23 15:14:56 +0530

    condition for empty author string

--- src/components/ArticleDetail.js
+++ src/components/ArticleDetail.js
@@ -9,6 +9,24 @@ const ArticleDetail=({article})=>{
 </div>
 );
     }
+    if(!article.author){
+        return(
+            <div><div className="ui inverted segment">
+            <h2 className="ui header" id="articletitle">{article.title}</h2>
+            <h3 className="ui header" id="article">{article.description}</h3>
+            <br/>
+            <div className="ui grid"><div className="ui row">
+            <div className="eleven wide column"><h5 className="ui header" id="date">{article.publishedAt}</h5></div>
+            <div className="five wide column"><h5 className="ui header" id="author">{article.author}</h5></div>
+            </div>
+            </div>
+            
+            <div className="ui divider"></div>
+            <img className="ui large image" src={article.urlToImage} alt="" id="detailimg"/><br/>
+            <p id="article">{article.content}</p>
+            </div></div>
+        );
+    }
 return(
 <div>  
 <div className="ui inverted segment">
